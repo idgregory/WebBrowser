b31fbae2979e234f54ccd97191e707e37e6896ad
idgregory <60363703+idgregory@users.noreply.github.com>
2020-01-28 17:23:35 -0600

    History Done, but no functionality

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -36,14 +36,16 @@
             this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // go_btn
             // 
-            this.go_btn.Location = new System.Drawing.Point(304, 80);
+            this.go_btn.Location = new System.Drawing.Point(456, 123);
+            this.go_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.go_btn.Name = "go_btn";
-            this.go_btn.Size = new System.Drawing.Size(29, 23);
+            this.go_btn.Size = new System.Drawing.Size(44, 35);
             this.go_btn.TabIndex = 0;
             this.go_btn.Text = "go";
             this.go_btn.UseVisualStyleBackColor = true;
@@ -51,26 +53,29 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 80);
+            this.textBox1.Location = new System.Drawing.Point(18, 123);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(286, 20);
+            this.textBox1.Size = new System.Drawing.Size(427, 26);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "youb";
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(12, 106);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Location = new System.Drawing.Point(18, 163);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(30, 31);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(934, 501);
+            this.webBrowser1.Size = new System.Drawing.Size(1401, 771);
             this.webBrowser1.TabIndex = 2;
             // 
             // back_btn
             // 
-            this.back_btn.Location = new System.Drawing.Point(13, 51);
+            this.back_btn.Location = new System.Drawing.Point(20, 78);
+            this.back_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.back_btn.Name = "back_btn";
-            this.back_btn.Size = new System.Drawing.Size(75, 23);
+            this.back_btn.Size = new System.Drawing.Size(112, 35);
             this.back_btn.TabIndex = 3;
             this.back_btn.Text = "Back";
             this.back_btn.UseVisualStyleBackColor = true;
@@ -78,9 +83,10 @@
             // 
             // prev_btn
             // 
-            this.prev_btn.Location = new System.Drawing.Point(94, 51);
+            this.prev_btn.Location = new System.Drawing.Point(141, 78);
+            this.prev_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.prev_btn.Name = "prev_btn";
-            this.prev_btn.Size = new System.Drawing.Size(75, 23);
+            this.prev_btn.Size = new System.Drawing.Size(112, 35);
             this.prev_btn.TabIndex = 4;
             this.prev_btn.Text = "Prev";
             this.prev_btn.UseVisualStyleBackColor = true;
@@ -88,26 +94,37 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.historyToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(958, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1437, 35);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // historyToolStripMenuItem
             // 
+            this.historyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
             this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
-            this.historyToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(81, 29);
             this.historyToolStripMenuItem.Text = "History";
             this.historyToolStripMenuItem.Click += new System.EventHandler(this.historyToolStripMenuItem_Click);
             // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 33);
+            this.toolStripComboBox1.Text = "Scroll Down";
+            this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
             // WebBrowser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(958, 619);
+            this.ClientSize = new System.Drawing.Size(1437, 952);
             this.Controls.Add(this.prev_btn);
             this.Controls.Add(this.back_btn);
             this.Controls.Add(this.webBrowser1);
@@ -115,6 +132,7 @@
             this.Controls.Add(this.go_btn);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "WebBrowser";
             this.Text = "WebBrowser";
             this.menuStrip1.ResumeLayout(false);
@@ -134,6 +152,7 @@
         private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -13,16 +13,21 @@ namespace WindowsFormsApplication1
     public partial class WebBrowser : Form
     {
         private WebSearch ws;
+        private String curr_string;
         public WebBrowser()
         {
             InitializeComponent();
             ws = new WebSearch(webBrowser1);
+            curr_string = null;
+
         }
-        private void formatString(String s)
+        private void formatString()
         {
-            if (!s.StartsWith("http://") && !s.StartsWith("https://"))
+            if (String.IsNullOrEmpty(curr_string)) return;
+            if (curr_string.Equals("about:blank")) return;
+            if (!curr_string.StartsWith("http://") && !curr_string.StartsWith("https://"))
             {
-                s = "http://" + s;
+                curr_string = "http://" + curr_string;
             }
         }
 
@@ -30,38 +35,55 @@ namespace WindowsFormsApplication1
         private void search_btn_Click(object sender, EventArgs e)
         {
 
-            String s = textBox1.Text;
-            formatString(s);
-            ws.search(s, true);
-            historyToolStripMenuItem.DropDownItems.Add(s);
+            curr_string = textBox1.Text;
+            formatString();
+            ws.search(curr_string, true);
+            toolStripComboBox1.Items.Add(curr_string);
         }
 
         private void textBox1_Enter(object sender, EventArgs e)
         {
-            String s = textBox1.Text;
-            formatString(s);
-            ws.search(s, true);
+            curr_string = textBox1.Text;
+            formatString();
+            ws.search(curr_string, true);
+            toolStripComboBox1.Items.Add(curr_string);
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter)
             {
-                ws.search(textBox1.Text, true);
+                curr_string = textBox1.Text;
+                formatString();
+                ws.search(curr_string, true);
+                toolStripComboBox1.Items.Add(curr_string);
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            curr_string = textBox1.Text;
+            formatString();
+            toolStripComboBox1.Items.Add(curr_string);
             ws.search_back();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+            curr_string = textBox1.Text;
+            formatString();
+            toolStripComboBox1.Items.Add(curr_string);
             ws.search_prev();
         }
 
         private void historyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
         {
 
         }
